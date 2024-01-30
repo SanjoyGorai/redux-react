dae83104d0a6cf83d5a1cdb702d0cc17b43de0a4
Mr. Sanjoy <--add>
2024-01-30 20:01:18 +0530

    redux react

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment } from './features/counter/counterSlice'
+
+
+const App = () => {
+
+
+  const value = useSelector (s => s.counter.value);
+
+  function handleClick(e) {
+    switch (e.target.id) {
+      case 'increment':
+        console.log(e.target.id);
+        
+        break;
+      case 'decrement':
+
+        console.log(e.target.id);
+        break;
+    }
+  }
+
+  return (
+    <div className='gap-3'>
+      <h1>Redux React App</h1>
+      <h2 className='font-bold m-3 text-xl'> Value : {value} </h2>
+      <button className='bg-green-500 m-2' id='increment' onClick={handleClick}>Increment</button>
+      <button className='bg-red-700' id='decrement' onClick={handleClick}>Decrement</button>
+    </div>
+  )
+}
+
+export default App
